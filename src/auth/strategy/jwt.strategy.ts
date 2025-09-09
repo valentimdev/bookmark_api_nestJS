d75d4c3eeaece2b0a15c7f22dd1472651a5277df
valentimdev <valentimdesenvolvedor@gmail.com>
2025-09-09 15:16:20 -0300

    custom decorator get user

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,14 @@ import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  prisma: any;
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService,
+    private prisma: PrismaService
+  ) {
     const secret = config.get('JWT_SECRET');
 
     super({
@@ -15,8 +18,21 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       secretOrKey: secret,
     });
   }
-  async validate(payload: any) {
-    console.log(payload);
-    return payload;
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    const { hash, ...result } = user;
+    return result;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import type { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('users')
 export class UserController {
-    @UseGuards(AuthGuard('jwt'))
-    @Get('me')
-    getMe(){
-        return 'oi';
-    }
-}
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+} 
